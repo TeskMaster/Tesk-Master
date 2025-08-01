ca1f38f2e7d56f40bfef50509a4c589f9a827137
Marcos Tulio <marcos.age07@gmail.com>
2025-08-01 20:36:46 -0300

    Teste de usuário funcionando(falta reconfigurar os outros que já estão criados)

---
+++ config-service/criar-usuarios/src/test/java/com/microservices_esofii/CriarUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.microsservices_esofii.crud_de_eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriarUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriarUsuariosApplication.class, args);
+	}
+
+}
